2daa71982f21d6af57fc78c46cef70166baff920
jklinges <70554411+jklinges@users.noreply.github.com>
2020-12-20 16:30:10 -0500

    Update app.js

--- static/JS/app.js
+++ static/JS/app.js
@@ -4,7 +4,7 @@ var inputField1 = d3.select("#datetime");
 var inputField2 = d3.select("#city");
 var tbody = d3.select("tbody");
 var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments"]
 
 var populate = (dataInput) => {
 
